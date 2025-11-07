7d2cc706620b4c1e89be0f4fbfe5ee531325458e
James ‘Twey’ Kay <twey@twey.co.uk>
2025-11-07 13:04:44 +0000

    remove potentially dangerous command

--- run.bash
+++ run.bash
@@ -5,8 +5,6 @@ set -eu
 eval "$(linera net helper)"
 linera_spawn linera net up --with-faucet
 
-rm -rf ~/.config/linera
-
 export LINERA_FAUCET_URL=http://localhost:8080
 linera wallet init --faucet="$LINERA_FAUCET_URL"
 linera wallet request-chain --faucet="$LINERA_FAUCET_URL"
